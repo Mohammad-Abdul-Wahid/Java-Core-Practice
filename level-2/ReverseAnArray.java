3f39d59bd225cc96d853481092782ee2bf47897b
Wahid <mdabdulwahid2004@gmail.com>
2026-03-10 23:54:37 +0530

    Lvele-2 problem-3

---
+++ level-2/ReverseAnArray.java
@@ -0,0 +1,12 @@
+public class ReverseAnArray{
+  public static void main(String[] args) {
+    int arr[]={5,4,3,2,1};
+    int rev[] = new int[5];
+    int j=0;
+    for(int i=arr.length-1;i>=0;i--){
+      rev[j]=arr[i];
+      j++;
+    }
+
+  }
+}
