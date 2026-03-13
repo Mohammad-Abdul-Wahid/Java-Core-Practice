a6fb9337f5b7e7be4ae5e2305845556c91d93ed3
Wahid <mdabdulwahid2004@gmail.com>
2026-03-13 23:32:58 +0530

    Count words of a string problem

---
+++ level-2/CountWords.java
@@ -0,0 +1,8 @@
+public class CountWords {
+  public static void main(String[] args) {
+    String str="My name is wahid";
+    String arr[]=str.split(" ");
+    System.out.println(arr.length);
+  }
+  
+}
