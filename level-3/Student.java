0a347bfba280b567bc1c294c1a42050c2fd71145
Wahid <mdabdulwahid2004@gmail.com>
2026-03-13 23:43:27 +0530

    OOPs level question in lenel-3

---
+++ level-3/Student.java
@@ -0,0 +1,26 @@
+
+public class Student {
+  String name;
+  int roll_number;
+  int age;
+
+  public Student(String name, int roll_number, int age) {
+    this.name = name;
+    this.roll_number = roll_number;
+    this.age = age;
+  }
+
+  public String getName(String name){
+    return this.name;
+  }
+  public int getRollNumber(String name){
+    return this.roll_number;
+  }
+  public int getAgel(String name){
+    return this.age;
+  }
+
+  public static void main(String[] args) {
+
+  }
+}
