1c94bc46abd3adada553575b89aba6b48deb54c5
Wahid <mdabdulwahid2004@gmail.com>
2026-03-14 23:14:42 +0530

    Problem-3 level 3

---
+++ level-3/Constructor.java
@@ -0,0 +1,21 @@
+public class Constructor {
+  public Constructor(){
+
+  }
+  public Constructor(int a){
+
+  }
+  public Constructor(float a){
+
+  }
+  public Constructor(int a,float b){
+    
+  }
+  public Constructor(float a,int b){
+
+  }
+  public static void main(String[] args) {
+    
+  }
+  
+}
