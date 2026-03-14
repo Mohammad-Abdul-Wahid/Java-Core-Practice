ccc09441a155cfe835f01351fd7d57675824f477
Wahid <mdabdulwahid2004@gmail.com>
2026-03-14 23:12:30 +0530

    Problem-2 level 3

--- level-3/OverLoading.java
+++ level-3/OverLoading.java
@@ -1,5 +1,22 @@
-public class OverLoading{
+public class OverLoading {
+
+  public void print(int a) {
+
+  }
+
+  public void print(int a, int b) {
+
+  }
+
+  public void print(float a, int b) {
+
+  }
+
+  public void print(int a, float b) {
+
+  }
+
   public static void main(String[] args) {
-    
+
   }
 }
