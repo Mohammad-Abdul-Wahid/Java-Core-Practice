b472bd6ce986e6172871f48df520116528c2e190
Wahid <mdabdulwahid2004@gmail.com>
2026-03-14 23:18:43 +0530

    Problem-4 level 3

---
+++ level-3/Inheritance.java
@@ -0,0 +1,18 @@
+class Person {
+  public void eat() {
+    System.out.println("Eating...");
+  }
+}
+
+class Employee extends Person {
+  public void work() {
+    System.out.println("Working");
+  }
+}
+
+public class Inheritance {
+  public static void main(String[] args) {
+
+  }
+
+}
