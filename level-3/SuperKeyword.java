1c1d4310dd26c5615e86f1e548f7b22c7d85640a
Wahid <mdabdulwahid2004@gmail.com>
2026-03-16 19:37:55 +0530

    Modifying level-3 problem

--- level-3/SuperKeyword.java
+++ level-3/SuperKeyword.java
@@ -1,16 +1,14 @@
+class Animal{
+  public Animal(){
+    System.out.println("Inside Animal class constructor");
+  }
+}
+
+
 public class SuperKeyword {
 
-  public SuperKeyword(){
-    System.out.println("Inside default constructor");
-  }
-
-  public SuperKeyword(int a){
-    super();
-    System.out.println("Inside parametrized constructor");
-  }
-
   public static void main(String[] args) {
-    SuperKeyword sk=new SuperKeyword(1);
+
   }
 
   }
