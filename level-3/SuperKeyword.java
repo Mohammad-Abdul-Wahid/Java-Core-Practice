ffb246423e1650aa4b8a7390e8f6f62ade7bcae6
Wahid <mdabdulwahid2004@gmail.com>
2026-03-16 19:43:32 +0530

    Level-3 problem-5

--- level-3/SuperKeyword.java
+++ level-3/SuperKeyword.java
@@ -3,9 +3,17 @@ class Animal{
     System.out.println("Inside Animal class constructor");
   }
 }
-class Dog{
+class Dog extends Animal{
   public Dog(){
+    super();
     System.out.println("Inside dog class constructor");
+  }
+}
+
+class BullDog extends Dog{
+  public BullDog(){
+    super();
+    System.out.println("Inside BullDog class constructor");
   }
 }
 
@@ -13,7 +21,7 @@ class Dog{
 public class SuperKeyword {
 
   public static void main(String[] args) {
-
+    BullDog bd=new BullDog();
   }
 
   }
