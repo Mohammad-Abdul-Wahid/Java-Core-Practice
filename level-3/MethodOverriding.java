c706dcd50525761a9b9ee0d8aba4a8d049760c95
Wahid <mdabdulwahid2004@gmail.com>
2026-03-17 19:49:49 +0530

    Level-3 problem 6

---
+++ level-3/MethodOverriding.java
@@ -0,0 +1,13 @@
+
+class Animal {
+  public void sound() {
+    System.out.println("Animal makes sound.");
+  }
+}
+
+
+public class MethodOverriding {
+  public static void main(String[] args) {
+
+  }
+}
