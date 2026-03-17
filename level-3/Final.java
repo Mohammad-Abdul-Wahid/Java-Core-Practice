2981cde04453d17564e3dd3abd7b6d4b67dccd7d
Wahid <mdabdulwahid2004@gmail.com>
2026-03-17 20:03:07 +0530

    Level-3 problem 7

---
+++ level-3/Final.java
@@ -0,0 +1,12 @@
+//Cannot extend this class
+public final class Final {
+  public static void main(String[] args) {
+    final int a = 4;// Cannot change this variable
+
+  }
+
+  // Cannot be overriden this method
+  public final void method() {
+
+  }
+}
